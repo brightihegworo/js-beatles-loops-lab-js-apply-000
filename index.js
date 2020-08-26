90a885d828ff436c42dc7f882498cc3fe41caf1a
Bright It's <brightihegworo52@gmail.com>
2020-08-26 15:48:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  const arr = [];
+  
+  for(var i = 0; i < musicians.length; i ++) {
+   arr.push(musicians[i] + " plays " + instruments[i]) ;
+  }
+  
+  return arr;
+}
+
+function johnLennonFacts() {
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+const arr = [];
+
+while(arr.length < facts.length) {
+ arr.push(facts[i] + '!!!') 
+}
+
+return arr;
+}
+
+function iLoveTheBeatles(number) {
+  const arr = [];
+  
+  do {
+    array.push("I love the Beatles!");
+  } while (number < 15) {
+    return arr;
+  }
+  
+}
